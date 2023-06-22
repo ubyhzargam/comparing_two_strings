76315aa6d02964e9fc607b56dfe6dd1747e18ad0
ubyhzargam <ubyhzargam@gmail.com>
2023-06-22 18:26:33 +0530

    Update main.cpp

--- comparing_two_strings/main.cpp
+++ comparing_two_strings/main.cpp
@@ -6,9 +6,27 @@
 //
 
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+using namespace std;
+int main()
+{
+    char str1[50],str2[50];
+    cout<<" This code compares two strings and prints whether they are equal or not ";
+    cout<<"Enter first string : "<<endl;
+    cin>>str1;
+    cout<<"Enter second string : "<<endl;
+    cin>>str2;
+    for(int i=0; i<50;i++)
+    {
+        if(str1[i]=='\0'&&str2[i]=='\0')
+        {
+            cout<<"The given strings are equal"<<endl;
+            break;
+        }
+        else if((str1[i]=='\0'&&str2[i]!='\0')||(str1[i]!='\0'&&str2[i]=='\0')||(str1[i]!=str2[i]))
+        {
+            cout<<"The  given strings are not equal"<<endl;
+            break;
+        }
+    }
     return 0;
 }
